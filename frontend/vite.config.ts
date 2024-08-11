eed4f1dcb5ea1d78b6adf5364fb6b9c39a3ad8e8
Akshay Prabhakar <akshaysprabhakar@gmail.com>
2024-08-10 22:04:08 -0700

    deployy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/blogs': 'https://knowledge-dump-backend.vercel.app/',
+      '/blogs': 'https://knowledge-dump-backend.vercel.app',
     },
   },
 })
