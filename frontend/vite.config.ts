3f1b835b4570e2011377aa3510ad951baf865bf1
Akshay Prabhakar <akshaysprabhakar@gmail.com>
2024-08-17 11:39:21 -0700

    deploy again

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/blogs': 'http://localhost:6969',
+      "/blogs": "http://localhost:6969",
     },
   },
-})
+});
